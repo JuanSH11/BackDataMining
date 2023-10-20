147cf3f2fd3c7ec93b30ff90b1d835f8f93da9c1
AlejandroAhogado <a.ahogado@uniandes.edu.co>
2023-10-19 19:24:28 -0500

    Fix read_commits_by_user

--- views.py
+++ views.py
@@ -147,7 +147,7 @@ def create_issue(issue: schemas.IssueCreate, db: Session = Depends(get_db)):
 
 # Functions for the relationships between tables
 @app.get("/users/{user_id}/commits/", response_model=List[schemas.Commit])
-def read_commits_by_user(user_id: int, db: Session = Depends(get_db)):
+def read_commits_by_user(user_id: str, db: Session = Depends(get_db)):
     db_user = controllers.get_user(db, user_id=user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
